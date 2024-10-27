da149c0b4f5f46f64a286b7ab626e162b6bbb59e
cosm1cgirl <166348211+cosm1cgirl@users.noreply.github.com>
2024-10-27 12:06:11 +0200

    Updated App.js and css
    Background css added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a>
-          Weather Search Engine
-        </a>
-      </header>
-    </div>
-  );
+  return <a>Weather Search Engine</a>;
 }
 
 export default App;
